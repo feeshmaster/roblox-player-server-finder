666332492d1881af784e55cb818bc44fda813bc2
feeshmaster <125420779+feeshmaster@users.noreply.github.com>
2024-08-21 19:54:49 -0500

    test

---
+++ scripts/page-check.js
@@ -0,0 +1,9 @@
+if (window.location.hostname === "www.roblox.com") {
+    if (window.location.href.includes("/games")) {
+        
+    }
+}
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Document is fully loaded and parsed.");
+    console.log(document);  // Will log the entire document
+});
