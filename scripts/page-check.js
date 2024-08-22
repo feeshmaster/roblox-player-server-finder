8262d118b376062b2f14702c53e757a0e8e1c9ff
feeshmaster <125420779+feeshmaster@users.noreply.github.com>
2024-08-21 22:00:06 -0500

    progress

--- scripts/page-check.js
+++ scripts/page-check.js
@@ -1,9 +1,19 @@
-if (window.location.hostname === "www.roblox.com") {
-    if (window.location.href.includes("/games")) {
-        
+let content_location = '';
+document.addEventListener('DOMContentLoaded', function() {
+
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    let tab = tabs[0]
+    console.log(JSON.stringify(tab))
+    content_location = tab.url
+    let url = new URL(content_location);
+
+    if (url.hostname === "www.roblox.com") {
+        document.getElementById("default").style.display = 'none'
+        document.getElementById("app").style.display = 'block'
+        if (url.pathname.includes("/games")) {
+            document.getElementById("game-id").value = url.pathname.split('/')[2]
+            return
+        }
     }
-}
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Document is fully loaded and parsed.");
-    console.log(document);  // Will log the entire document
+  });
 });
